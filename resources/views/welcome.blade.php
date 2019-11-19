a93063e6f30b76aad03f5ce46453cceb739a42e2
momoko.shirai <mona11020325@gmail.com>
2019-11-19 09:15:13 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the TASK List!</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-primary btn-lg']) !!}
         </div>
     </div>
 
