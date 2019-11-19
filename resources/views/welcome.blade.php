d8141d74f6eb667674b10e133b7d213a2ce20ccd
momoko.shirai <mona11020325@gmail.com>
2019-11-19 09:41:15 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the TASK List!</h1>
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-primary btn-lg']) !!}
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the TASK List!</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-primary btn-lg']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 
 @endsection
